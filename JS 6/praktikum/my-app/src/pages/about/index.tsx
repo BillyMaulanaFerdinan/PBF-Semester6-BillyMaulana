734e25f8540eb124e3df9bd8cd0100eb0e7aa836
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2026-03-04 02:39:59 +0700

    initial commit JS 6

---
+++ JS 6/praktikum/my-app/src/pages/about/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <Head>
+        <title>Halaman About</title>
+      </Head>
+      <h1>Ini Halaman About</h1>  
+    </div>
+  );
+};
+
+export default About;
